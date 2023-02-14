9b7fbc8add1bbd7f09b4423b2f6edfa18379e999
Gangadhar Reddy <117704156+Gangadhar0022@users.noreply.github.com>
2023-02-14 11:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+const main = document.getElementById("main");
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+const guess = document.getElementById("guess").value;
+
+btn.addEventListener("click", function() {
+  const randomNumber = Math.floor(Math.random() * 41) - 20;
+  num.textContent = randomNumber;
+	if((guess-randomNumber)<=5){
+		document.getElementById("respond").innerText="Hot";
+	}
+	else{
+		document.getElementById("respond").innerText="Cold";
+		
+	}
+});
+
 
